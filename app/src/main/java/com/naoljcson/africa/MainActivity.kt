db22508c2ee6432a6089e7e18d53d9c0b1798230
Naol Hussien <naol.h@suntecholding.com>
2022-10-25 16:32:53 +0300

    Added: botom navigation

--- app/src/main/java/com/naoljcson/africa/MainActivity.kt
+++ app/src/main/java/com/naoljcson/africa/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.naoljcson.africa
 
+import android.app.UiModeManager
+import android.content.Context
+import android.os.Build
+import android.os.Build.VERSION_CODES.Q
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -12,9 +17,10 @@ import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         private val TAG = MainActivity::class.qualifiedName
     }
+
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
@@ -25,6 +31,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         setContentView(binding.root)
 //        storageReference.child("buffalo-1.jpg")
 //            .downloadUrl.addOnSuccessListener { uri ->
@@ -45,4 +55,5 @@ class MainActivity : AppCompatActivity() {
 //                }
 //            }
     }
+
 }
